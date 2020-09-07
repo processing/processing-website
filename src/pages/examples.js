d992ec0014a499635ef00395104cf5f04d7ccaa8
Anadroid <anajelicic89@gmail.com>
2020-09-07 12:54:36 +0200

    Adding categories to tutorials and examples

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import unique from 'array-unique';
 
 import { LocalizedLink as Link } from 'gatsby-theme-i18n';
 
@@ -9,17 +10,47 @@ import { useLocalization } from 'gatsby-theme-i18n';
 
 const Examples = ({ data }) => {
   const { locale } = useLocalization();
+
+  let examples = data.allFile.nodes;
+
+  let categories = unique(
+    examples.map((file) => {
+      return file.relativeDirectory.split('/')[0];
+    })
+  );
+
+  let subcategories = {};
+  categories.map((c) => {
+    subcategories[c] = unique(
+      examples.map((r) => {
+        if (r.relativeDirectory.split('/')[0] === c)
+          return r.relativeDirectory.split('/')[1];
+        else return null;
+      })
+    );
+  });
+
   return (
     <Layout>
       <h1>Examples</h1>
       <ul>
-        {data.allFile.nodes.map((node, key) => {
+        {categories.map((c, key) => {
+          let categoryRefs = examples.filter((ref) => {
+            return ref.relativeDirectory.split('/')[0] === c;
+          });
           return (
-            <li key={key}>
-              <Link to={node.childMdx.frontmatter.slug} language={locale}>
-                {node.childMdx.frontmatter.title}
-              </Link>
-            </li>
+            <div key={key}>
+              <h2>{c}</h2>
+              {categoryRefs.map((node, k) => {
+                return (
+                  <li key={k}>
+                    <Link to={node.childMdx.frontmatter.slug} language={locale}>
+                      {node.childMdx.frontmatter.title}
+                    </Link>
+                  </li>
+                );
+              })}
+            </div>
           );
         })}
       </ul>
@@ -39,6 +70,7 @@ export const query = graphql`
     ) {
       nodes {
         name
+        relativeDirectory
         childMdx {
           frontmatter {
             slug
