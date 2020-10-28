075317b5dbaf54a7dca7e52747264fbc58970d9b
Esteban <ejsandoval@uc.cl>
2020-10-28 00:47:19 -0300

    Fetching data with Graphql.

--- src/pages/download.js
+++ src/pages/download.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import classnames from 'classnames';
 import { graphql, useStaticQuery } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
@@ -6,18 +6,31 @@ import { DateTime } from 'luxon';
 
 import Layout from '../components/Layout';
 
-import { selectedReleases } from '../utils/constants';
+import {
+  selectedReleasesNumbers,
+  selectedPreReleasesNumbers,
+  versionLabels,
+} from '../utils/constants';
 
 import css from '../styles/pages/download.module.css';
 import grid from '../styles/grid.module.css';
-const preReleases = ['4.0a2', '4.0a1'];
+
+let latestAssets = [
+  { os: 'windows', bit: 64, label: 'Windows' },
+  { os: 'linux', bit: 64, label: 'Linux' },
+  { os: 'mac', label: 'Mac OS X' },
+  { os: 'windows', bit: 32, label: 'Windows' },
+];
 
 const Download = () => {
-  const { preRelease, github: githubData } = useStaticQuery(graphql`
+  const {
+    preReleases: preReleasesData,
+    releases: releasesData,
+  } = useStaticQuery(graphql`
     query {
-      github {
+      releases: github {
         repository(name: "processing", owner: "processing") {
-          releases(first: 100, orderBy: { field: NAME, direction: ASC }) {
+          releases(first: 100, orderBy: { field: NAME, direction: DESC }) {
             edges {
               node {
                 name
@@ -36,39 +49,75 @@ const Download = () => {
           }
         }
       }
-      preRelease {
-        url
-        name
-        tagName
-        published
-        assets {
-          name
-          content_type
-          browser_download_url
+      preReleases: github {
+        repository(name: "processing4", owner: "processing") {
+          releases(first: 100, orderBy: { field: NAME, direction: DESC }) {
+            edges {
+              node {
+                name
+                tagName
+                publishedAt
+                releaseAssets(first: 10) {
+                  edges {
+                    node {
+                      name
+                      downloadUrl
+                    }
+                  }
+                }
+              }
+            }
+          }
         }
       }
     }
   `);
 
-  const releases = githubData.repository.releases.edges.map((e) => e.node);
-  const latestRelease = releases.pop();
-  let latestAssets = [
-    { os: 'windows', bit: 64, label: 'Windows' },
-    { os: 'mac', label: 'Mac OS X' },
-    { os: 'linux', bit: 64, label: 'Linux' },
-    { os: 'linux', bit: 32, label: 'Linux' },
-    { os: 'windows', bit: 32, label: 'Windows' },
-  ];
+  const releases = releasesData.repository.releases.edges.map((e) => e.node);
+  const selectedReleases = releases.filter((r) =>
+    selectedReleasesNumbers.includes(r.name.split(' ').pop())
+  );
+  const preReleases = preReleasesData.repository.releases.edges.map(
+    (e) => e.node
+  );
+  const selectedPreReleases = preReleases.filter((r) =>
+    selectedPreReleasesNumbers.includes(r.tagName.split('-').pop())
+  );
+
+  const latestRelease = releases.shift();
+
+  const createLabel = (filename) => {
+    let label = '';
+    versionLabels.forEach((item, i) => {
+      if (filename.includes(item.os)) label = label + item.label;
+    });
+    if (filename.includes('64')) label = label + ' 64';
+    else if (filename.includes('32')) label = label + ' 32';
+    else if (
+      !versionLabels
+        .map((e) => e.os)
+        .some((os) => filename.split('-').pop().includes(os))
+    )
+      label = label + ` ${filename.split('-').pop().slice(0, -4)}`;
+    return label;
+  };
+
   latestAssets = latestAssets.map((latestAsset) => {
     const assetsData = latestRelease.releaseAssets.edges.map((e) => e.node);
-    for(let a of assetsData)  {
-      const { filename: name, downloadUrl } = a;
-      if (filename.includes(latestAsset.os) && filename.includes(latestAsset.bit)) latestAsset['url'] = a.downloadUrl;
-      else if (filename.includes(latestAsset.os) && !filename.includes(latestAsset.bit)) latestAsset['url'] = a.downloadUrl;
-    });
-
-    const match = assetsData.filter((asset) => {
-      const name = assetsData.name.split('-').pop();
+    for (let a of assetsData) {
+      const { name: filename, downloadUrl } = a;
+      if (latestAsset.bit) {
+        if (
+          filename.includes(latestAsset.os) &&
+          filename.includes(latestAsset.bit)
+        )
+          latestAsset['url'] = a.downloadUrl;
+        else latestAsset['url'] = a.downloadUrl;
+      } else if (filename.includes(latestAsset.os))
+        latestAsset['url'] = a.downloadUrl;
+    }
+    const match = assetsData.find((asset) => {
+      const name = asset.name.split('-').pop();
       if (name.includes(latestAsset.os) && !latestAsset.bit) return true;
       else if (
         asset.name.includes(latestAsset.os) &&
@@ -77,8 +126,8 @@ const Download = () => {
         return true;
       else return false;
     });
-    if (match.length > 0) latestAsset['url'] = match.shift().downloadUrl;
-    return version;
+    if (match) latestAsset['url'] = match.downloadUrl;
+    return latestAsset;
   });
 
   return (
@@ -89,7 +138,8 @@ const Download = () => {
           Processing is available for Linux, Mac OS X, and Windows. Select your
           choice to download the software below.
         </h3>
-        <div className={classnames(grid.nest, grid.col6, grid.grid)}>
+        <div
+          className={classnames(grid.nest, grid.col6, grid.grid, css.section)}>
           <div className={classnames(css.logo, grid.col2)}>
             <h2>{latestRelease.name.split(' ').shift()}</h2>
           </div>
@@ -114,7 +164,7 @@ const Download = () => {
                 key={`version-${i}`}
                 className={classnames(grid.col1, css.latestVersion)}>
                 <a href={v.url}>
-                  <span className={css.latestVersionName}>{v.name}</span>
+                  <span className={css.latestVersionName}>{v.label}</span>
                   {v.bit && (
                     <span
                       className={css.latestVersionBit}>{`${v.bit}-bit`}</span>
@@ -147,14 +197,54 @@ const Download = () => {
               </a>
             </li>
           </ul>
-          <p className={grid.col5}>
-            Read about the changes in 3.0. The list of revisions covers the
-            differences between releases in detail.
-          </p>
         </div>
+        <p className={grid.col5}>
+          Read about the changes in 3.0. The list of revisions covers the
+          differences between releases in detail.
+        </p>
         <div
-          className={classnames(grid.nest, grid.col5, grid.grid, grid.push1)}>
-          <h3>Stable Releases</h3>
+          className={classnames(grid.nest, grid.col6, grid.push1, css.section)}>
+          <h3 className={grid.col6}>Stable Releases</h3>
+          <ul className={css.releaseList}>
+            {selectedReleases.map((release) => (
+              <li className={css.releaseRow} key={release.name}>
+                <span className={grid.col1}>
+                  {release.name.replace('Processing ', '')}
+                </span>
+                <span
+                  className={classnames(
+                    grid.col1,
+                    css.releaseDate
+                  )}>{` (${DateTime.fromISO(release.publishedAt).toLocaleString(
+                  DateTime.DATE_FULL
+                )})`}</span>
+                <span className={grid.col4}>
+                  {release.releaseAssets.edges
+                    .sort((a, b) =>
+                      a.node.name > b.node.name
+                        ? -1
+                        : a.node.name < b.node.name
+                        ? 1
+                        : 0
+                    )
+                    .map((asset, i) => {
+                      const { name: filename, downloadUrl } = asset.node;
+                      const label = createLabel(filename);
+                      return (
+                        <Fragment key={`asset-container-${i}`}>
+                          <a href={downloadUrl} key={`preRelease-asset-${i}`}>
+                            <span className={css.assetLabel}>{label}</span>
+                          </a>
+                          {i < release.releaseAssets.edges.length - 1 && (
+                            <span className={css.assetSeparator}>/</span>
+                          )}
+                        </Fragment>
+                      );
+                    })}
+                </span>
+              </li>
+            ))}
+          </ul>
         </div>
         <p className={grid.col5}>
           Earlier releases have been removed because we can only support the
@@ -189,26 +279,47 @@ const Download = () => {
           Processing 3 or later is required.
         </p>
         <div
-          className={classnames(grid.nest, grid.col5, grid.grid, grid.push1)}>
-          <h3>Pre-Releases</h3>
+          className={classnames(grid.nest, grid.col6, grid.push1, css.section)}>
+          <h3 className={grid.col6}>Pre-Releases</h3>
           <ul className={css.releaseList}>
-            <li className={css.releaseRow}>
-              <span className={grid.col1}>
-                {preRelease.name.replace('Processing ', '')}
-              </span>
-              <span className={grid.col1}>{` (${DateTime.fromISO(
-                preRelease.published
-              ).toLocaleString(DateTime.DATE_FULL)})`}</span>
-              <span className={grid.col3}>
-                {preRelease.assets.map((asset, i) => (
-                  <a
-                    href={asset.browser_download_url}
-                    key={`preRelease-asset-${i}`}>
-                    <span>{asset.name.split('-').pop()}</span>
-                  </a>
-                ))}
-              </span>
-            </li>
+            {selectedPreReleases.map((preRelease) => (
+              <li className={css.releaseRow} key={preRelease.name}>
+                <span className={grid.col1}>
+                  {preRelease.name.replace('Processing ', '')}
+                </span>
+                <span
+                  className={classnames(
+                    grid.col1,
+                    css.releaseDate
+                  )}>{` (${DateTime.fromISO(
+                  preRelease.publishedAt
+                ).toLocaleString(DateTime.DATE_FULL)})`}</span>
+                <span className={grid.col4}>
+                  {preRelease.releaseAssets.edges
+                    .sort((a, b) =>
+                      a.node.name > b.node.name
+                        ? -1
+                        : a.node.name < b.node.name
+                        ? 1
+                        : 0
+                    )
+                    .map((asset, i) => {
+                      const { name: filename, downloadUrl } = asset.node;
+                      const label = createLabel(filename);
+                      return (
+                        <Fragment key={`asset-container-${i}`}>
+                          <a href={downloadUrl} key={`preRelease-asset-${i}`}>
+                            <span className={css.assetLabel}>{label}</span>
+                          </a>
+                          {i < preRelease.releaseAssets.edges.length - 1 && (
+                            <span className={css.assetSeparator}>/</span>
+                          )}
+                        </Fragment>
+                      );
+                    })}
+                </span>
+              </li>
+            ))}
           </ul>
         </div>
         <p className={grid.col5}>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,34 +1,9 @@
-export const selectedReleases = [
-  {
-    number: '3.5.3',
-    versions: [
-      { os: 'windows', bit: 64, name: 'Win 64' },
-      { os: 'windows', bit: 32, name: 'Win 32' },
-      { os: 'linux', bit: 64, name: 'Linux 64' },
-      { os: 'linux', bit: 32, name: 'Linux 32' },
-      { os: 'linux', bit: 'armv6hf', name: 'Linux ARMv6hf' },
-      { os: 'max', name: 'Mac OS X' },
-    ],
-  },
-  {
-    number: '2.2.1',
-    versions: [
-      { os: 'windows', bit: 64, name: 'Win 64' },
-      { os: 'windows', bit: 32, name: 'Win 32' },
-      { os: 'linux', bit: 64, name: 'Linux 64' },
-      { os: 'linux', bit: 32, name: 'Linux 32' },
-      { os: 'max', name: 'Mac OS X' },
-    ],
-  },
-  {
-    number: '1.5.1',
-    versions: [
-      { os: 'windows', bit: 64, name: 'Win (standard)' },
-      { os: 'windows', bit: 'expert', name: 'Win (no Java)' },
-      { os: 'linux', name: 'Linux x86' },
-      { os: 'mac', name: 'Mac OS X' },
-    ],
-  },
-];
+export const selectedReleasesNumbers = ['3.5.3', '2.2.1', '1.5.1'];
 
-export const preReleasesVersion = {};
+export const selectedPreReleasesNumbers = ['4.0a2', '4.0a1'];
+
+export const versionLabels = [
+  { os: 'windows', label: 'Windows' },
+  { os: 'linux', label: 'Linux' },
+  { os: 'mac', label: 'Mac OS X' },
+];
