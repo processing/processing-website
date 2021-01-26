41e7af9410b75562b6644c686364242c3daa734d
Esteban <ejsandoval@uc.cl>
2021-01-26 20:48:23 -0300

    Updating Footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,12 +9,33 @@ import css from './Footer.module.css';
 const Footer = ({ siteTitle }) => {
   const intl = useIntl();
   return (
-    <footer className={css.root}>
-      <span
-        className={classnames(grid.grid, css.span)}
-        dangerouslySetInnerHTML={{
-          __html: intl.formatMessage({ id: 'footer' }),
-        }}></span>
+    <footer className={classnames(css.root, grid.grid)}>
+      <div className={classnames(grid.col, css.contactWrapper)}>
+        <h3>{intl.formatMessage({ id: 'contactUs' })}</h3>
+        <p>{intl.formatMessage({ id: 'contactUsDescription' })}</p>
+        <a href="mailto:foundation@processing.org">foundation@processing.org</a>
+      </div>
+      <div className={classnames(grid.col, css.socialmediaWrapper)}>
+        <ul>
+          <li>
+            <a href={'#'}>Twitter</a>
+          </li>
+          <li>
+            <a href={'#'}>Medium</a>
+          </li>
+          <li>
+            <a href={'#'}>Instagram</a>
+          </li>
+          <li>
+            <a href={'#'}>GitHub</a>
+          </li>
+        </ul>
+        <span
+          className={classnames(css.span)}
+          dangerouslySetInnerHTML={{
+            __html: intl.formatMessage({ id: 'footer' }),
+          }}></span>
+      </div>
     </footer>
   );
 };
