98c5c88a2940214598b08a0e96ab160fdba819df
Rune Madsen <rune@runemadsen.com>
2021-04-05 13:16:56 +0200

    fixing so no subcats are shown in reference

--- src/hooks/reference.js
+++ src/hooks/reference.js
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import { titleCase } from '../utils/data';
 
 /**
   Hook to turn the reference items in an object that can be used in useTree
@@ -6,6 +7,13 @@ import { useMemo } from 'react';
 **/
 export const usePreparedReferenceItems = (items) => {
   return useMemo(() => {
-    return items.map((item) => item.childJson);
+    // This makes up for some weirdness in lowercase/uppercase category and subcategory
+    // names and removes underscores and adds title cases.
+    return items.map((item) =>
+      Object.assign({}, item.childJson, {
+        category: titleCase(item.childJson.category),
+        subcategory: titleCase(item.childJson.subcategory),
+      })
+    );
   }, [items]);
 };
