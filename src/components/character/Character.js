d94039b5f3198c619c09cb190901e430d29717f6
Martin Bravo <bravomartin@gmail.com>
2021-07-01 16:36:47 -0400

    nicer donate, make character clickable

--- src/components/character/Character.js
+++ src/components/character/Character.js
@@ -1,4 +1,6 @@
 import React, { Fragment, useState, useEffect, useMemo } from 'react';
+import { Link } from 'gatsby';
+
 import classnames from 'classnames';
 
 import Eye from './Eye';
@@ -7,7 +9,7 @@ import css from './Character.module.css';
 
 import characters from './characters.json';
 
-const Character = ({ className }) => {
+const Character = ({ className, href }) => {
   const [version, setVersion] = useState(0);
 
   useEffect(() => {
@@ -47,48 +49,50 @@ const Character = ({ className }) => {
     setMoving(false);
   };
 
-  return (
-    <div className={classnames(css.root, { [className]: className })}>
-      <svg
-        viewBox={'0 0 150 150'}
-        onMouseEnter={registerEnter}
-        onMouseLeave={registerLeave}>
-        {shapes.map((shape, index) => {
-          let dPoints;
-          if (!shape.type) {
-            dPoints = shape.pos.map((x) => x);
-            dPoints.splice(0, 0, 'M');
-            dPoints.splice(3, 0, 'C');
-          }
+  const char = (
+    <svg
+      viewBox={'0 0 150 150'}
+      onMouseEnter={registerEnter}
+      onMouseLeave={registerLeave}>
+      {shapes.map((shape, index) => {
+        let dPoints;
+        if (!shape.type) {
+          dPoints = shape.pos.map((x) => x);
+          dPoints.splice(0, 0, 'M');
+          dPoints.splice(3, 0, 'C');
+        }
 
-          return shape.type === true ? (
-            <line
+        return shape.type === true ? (
+          <line
+            key={index}
+            x1={shape.pos[0]}
+            y1={shape.pos[1]}
+            x2={shape.pos[6]}
+            y2={shape.pos[7]}
+            stroke={shape.color}
+            strokeWidth={strokeWidth}
+          />
+        ) : (
+          <Fragment key={index}>
+            <path
               key={index}
-              x1={shape.pos[0]}
-              y1={shape.pos[1]}
-              x2={shape.pos[6]}
-              y2={shape.pos[7]}
+              d={dPoints.join(' ')}
               stroke={shape.color}
+              fill="none"
               strokeWidth={strokeWidth}
             />
-          ) : (
-            <Fragment key={index}>
-              <path
-                key={index}
-                d={dPoints.join(' ')}
-                stroke={shape.color}
-                fill="none"
-                strokeWidth={strokeWidth}
-              />
-            </Fragment>
-          );
-        })}
-        {eyes.map((eye, index) => {
-          return (
-            <Eye key={index + 'ey'} eye={eye} blink={blink} index={index} />
-          );
-        })}
-      </svg>
+          </Fragment>
+        );
+      })}
+      {eyes.map((eye, index) => {
+        return <Eye key={index + 'ey'} eye={eye} blink={blink} index={index} />;
+      })}
+    </svg>
+  );
+
+  return (
+    <div className={classnames(css.root, { [className]: className })}>
+      {href ? <Link to={href}>{char}</Link> : char}
     </div>
   );
 };
