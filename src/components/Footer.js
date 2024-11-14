279700acc1b2adeadc49b81f57fd9fdfa911a3c3
Raphaël de Courville <groupes.raphael@gmail.com>
2024-11-14 14:17:36 +0100

    Update social media links in Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,16 +20,16 @@ const Footer = ({ withSidebar }) => {
       <div className={classnames(grid.col, css.socialmediaWrapper)}>
         <ul>
           <li>
-            <a href={'https://twitter.com/ProcessingOrg'} target="_blank" rel="noreferrer">Twitter</a>
+            <a href={'https://twitter.com/ProcessingCore'} target="_blank" rel="noreferrer">Twitter</a>
           </li>
           <li>
             <a href={'https://medium.com/@ProcessingOrg'} target="_blank" rel="noreferrer">Medium</a>
           </li>
           <li>
-            <a href={'https://www.instagram.com/processingorg/'} target="_blank" rel="noreferrer">Instagram</a>
+            <a href={'https://www.instagram.com/processing_core/'} target="_blank" rel="noreferrer">Instagram</a>
           </li>
           <li>
-            <a href={'http://github.com/processing/'} target="_blank" rel="noreferrer">GitHub</a>
+            <a href={'http://github.com/processing/processing4/'} target="_blank" rel="noreferrer">GitHub</a>
           </li>
         </ul>
         <p
