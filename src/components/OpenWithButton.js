e6a950c7e61dadce8147c6248993149ef8612142
Stef Tervelde <stef@steftervelde.nl>
2025-01-26 22:11:45 +0100

    a tag to prevent opening window

--- src/components/OpenWithButton.js
+++ src/components/OpenWithButton.js
@@ -6,18 +6,15 @@ import ProcessingIcon from '../images/logo-processing.svg';
 import * as css from './CopyButton.module.css';
 
 const OpenWithButton = ({ text }) => {
-    const openWithProcessing = () => {
-        window.open('pde://sketch/base64/' + btoa(text));
-    };
-
     return (
-        <button
+        <a
+            href={`pde://sketch/base64/${btoa(text)}`}
             type="button"
             onClick={openWithProcessing}
             className={classnames(css.root)}
         >
             <ProcessingIcon /> {'Open With Processing'}
-        </button>
+        </a>
     )
 }
 
