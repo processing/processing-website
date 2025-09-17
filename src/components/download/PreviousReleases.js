3892501496de4e825cab67be8cff01d85d5ba5ed
Stef Tervelde <stef@steftervelde.nl>
2025-09-17 08:11:57 +0200

    Show Previous releases by Major and LTS version

--- src/components/download/PreviousReleases.js
+++ src/components/download/PreviousReleases.js
@@ -4,6 +4,7 @@ import * as semver from "semver";
 import { usePlatform } from "./Platform";
 import classNames from "classnames";
 import * as grid from 'styles/grid.module.css';
+import Button from "components/Button";
 
 
 
@@ -25,16 +26,16 @@ export default function PreviousReleases() {
         }
       }
 `)
-    const versions = releases.allFile.edges
+    const versionByMajor = releases.allFile.edges
         .map(e => e.node.childJson.tagName.replace(/^processing-(\d+-)?/, ''))
-        .map(e => [semver.coerce(e, { includePrerelease: true }), e])
+        .map(e => semver.coerce(e, { includePrerelease: true, raw: e }))
         .reverse()
-        .sort((a, b) => semver.compare(b[0], a[0]))
+        .sort((a, b) => semver.compare(b, a))
         // conver to object by major version
         .reduce((acc, cur) => {
-            const major = cur[0].major;
+            const major = cur.major;
             if (!acc[major]) acc[major] = [];
-            acc[major].push(cur[1]);
+            acc[major].push(cur);
             return acc;
         }, [])
         .reverse()
@@ -43,23 +44,29 @@ export default function PreviousReleases() {
 
 
     return (
-        <details style={{ flexBasis: 'var(--col8)' }} className={classNames(grid.col)}>
-            <summary>Looking for older versions?</summary>
-            {versions.map((v, i) => (
-                <details key={i}>
-                    <summary>Version {v[0].split('.')[0]} (latest {v[0]})</summary>
-                    <ul>
-                        {v.map((ver, j) => (
-                            <li key={j}>
-                                <a href={`/download/${platform.name}/${ver}`}>Processing {ver}</a>
-                            </li>)
-                        )}
-                    </ul>
-                </details>
-            ))}
-            <details>
-                <pre>{versions}</pre>
+        <div style={{ flexBasis: 'var(--col8)', background: "var(--lightgray);", paddingBlock: 20 }}>
+            <h4 style={{ paddingInline: 40 }}>Looking for older versions of Processing?</h4>
+            <details className={classNames(grid.col)}>
+                <summary style={{ paddingInline: 20 }}>Show</summary>
+                <div style={{ display: "flex", flexDirection: "row", gap: 20, paddingBlock: 10 }}>
+                    {versionByMajor.map((versions, i) => (
+                        <div key={i} style={{ paddingInline: 40, }}>
+                            <h4>Processing {versions[0].major}</h4>
+                            <Button href={`/download/${platform.name}/${versions[0].options.raw}`}>Processing {versions[0].options.raw}</Button>
+                            <details style={{ marginTop: 20 }}>
+                                <summary>More releases</summary>
+                                <ul>
+                                    {versions.map((version, j) => (
+                                        <li key={j}>
+                                            <a href={`/download/${platform.name}/${version.options.raw}`}>Processing {version.options.raw}</a>
+                                        </li>)
+                                    )}
+                                </ul>
+                            </details>
+                        </div>
+                    ))}
+                </div>
             </details>
-        </details>
+        </div>
     )
 }
