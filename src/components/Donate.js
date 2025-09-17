63a3426d3e2645fefb724e44dee2dc98c2b03c25
Stef Tervelde <stef@steftervelde.nl>
2025-09-17 09:22:29 +0200

    Add Donorbox donation widget to Donate component
    Integrated Donorbox by adding its widget script and embed iframe to the Donate component. Also applied custom styles and padding for improved layout.

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -1,11 +1,20 @@
 import classNames from "classnames"
-import React from "react"
+import React from "react";
 import * as grid from 'styles/grid.module.css';
+import * as css from 'styles/pages/donate.module.css';
 
 export default function Donate() {
     return (
-        <div style={{ flexBasis: 'var(--col4)', minHeight: 400, background: "#eee" }} className={classNames(grid.col)}>
+        <div style={{ flexBasis: 'var(--col4)', minHeight: 400, padding: "var(--gutter)" }} className={classNames(grid.col)}>
             Processing reached over 1 million people last year. If just 1% gave $5 a month, we’d raise $600,000 to support development. Your donation helps make Processing happen 💙
+            <script
+                src="https://donorbox.org/widget.js"
+                paypalexpress="true"></script>
+            <iframe
+                title={"Donate to Processing"}
+                className={css.donate}
+                allowpaymentrequest=""
+                src="https://donorbox.org/embed/support-processing?hide_donation_meter=true"></iframe>
         </div>
     )
 }
